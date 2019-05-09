249f94d3fc398ca6b570dd388c3b23a05305d76c
Alex Fairon <alex.fairon@gmail.com>
2019-05-09 09:18:32 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+
+using namespace std;
+using namespace web;
+using namespace web::http;
+using namespace utility;
+using namespace web::http::experimental::listener;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
