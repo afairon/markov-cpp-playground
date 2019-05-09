cb6c62ccf9bdeb46f61babeda1c2eb2629f965a3
Alex Fairon <alex.fairon@gmail.com>
2019-05-09 12:33:59 +0700

    add: GET api method
    Make a stub of GET api method.

--- src/controller.hpp
+++ src/controller.hpp
@@ -4,14 +4,22 @@
 #include <string>
 #include <vector>
 #include <cpprest/http_msg.h>
+#include <cpprest/http_listener.h>
+#include <pplx/pplxtasks.h>
 
 using namespace web;
 using namespace http;
+using namespace http::experimental::listener;
 
 class Controller {
+    protected:
+        http_listener _listener;
     public:
-        Controller() {}
-        ~Controller() {}
+        Controller(const std::string& addr) {
+            uri_builder uri(addr);
+            _listener = http_listener(uri.to_uri());
+        }
+        ~Controller() {};
         virtual void handleGet(http_request message) = 0;
         virtual void handlePost(http_request message) = 0;
         virtual void handlePut(http_request message) = 0;
@@ -19,8 +27,14 @@ class Controller {
         virtual void handlePatch(http_request message) = 0;
         virtual void handleHead(http_request message) = 0;
         virtual void handleOptions(http_request message) = 0;
+        
+        virtual void opHandlers() = 0;
 
         std::vector<std::string> requestPath(const http_request& message);
+
+        std::string uri() const;
+        pplx::task<void> accept();
+        pplx::task<void> shutdown();
 };
 
 #endif
